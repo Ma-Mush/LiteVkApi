6da4545890f8bae665428f2b37e60330fbd9843e
Ma-Mush <74294847+Ma-Mush@users.noreply.github.com>
2021-01-05 12:35:32 +0500

    Add files via upload

--- LiteVkApi/LiteVkApi/__init__.py
+++ LiteVkApi/LiteVkApi/__init__.py
@@ -1,3 +1,9 @@
+# =============================================================================
+# Придумал и разработал - MaMush (vk.com/maks.mushtriev2 , t.me/Error_mak25
+# Гитхаб - github.com/Ma-Mush/LiteVKApi
+# PypI - pypi.org/project/litevkapi/
+# =============================================================================
+
 class Vk():
     try:import vk_api
     except: raise ValueError('Ошибка авторизации (login):\n=====\nНе установлена библиотека vk_api - pip3 install vk_api\n=====')
@@ -9,21 +15,25 @@ class Vk():
     key = ''
     server = ''
     ts = ''
-    
+
     def help():
         print('     1. login <id_group> <token> <chats=False> <my_key=0> <my_server=0> <my_ts=0> - входит в бота по указанному ID группы и токену. Если бот для беседы - поставьте chats=True и укажите параметры подключения\n \
-2. msg <text> <userid> <chats=False> - отправляет текст по указанному ID пользователя/чата, если он уже писал боту/бот состоит в беседе и имеет право писать сообщения. \n \
-3. check_new_msg <chats=False> - проверяет наличие новых сообщений, выдает True/False. \n \
-4. get_event - возвращает в переменную данные о сообщении. \n \
-5. send_photo <file_name> <userid> <msg=None> <chats=False> - отправляет фото с компьютера по указанному адресу пользователю с указанным ID/беседе и сообщением (по умолчанию без него) при выполнении условий из функции "msg". \n \
-6. new_keyboard <dicts> <perm=True> - создает клавиатуру по массиву со словарями, например - [{\'кнопка 1\':\'цвет 1\'}, {\'new_line\':\' \'}, {\'кнопка 2\':\'цвет 2\'}], perm обозначает, будет ли пропадать клавиатура после нажатия на кнопку (при True - не будет). Подробнее можно узнать в документации - !!!ссылку вставить потом!!! \n \
-7. send_keyboard <keyboard> <userid> <msg=\'Клавиатура!\'> <chats=False> - отправляет пользователю/беседе по ID  указанную клавиатуру, отправляя указанное сообщение\n \
-8. delete_keyboard <userid> <msg=\'Клавиатура закрыта!\'> <chats=False> - удаляет клавиатуру у указанного пользователя, отправляя указанное сообщение\n \
-9. send_file <file_name> <userid> <msg=None> <chats=False> - отправляет файл с компьютера по указанному адресу пользователю с указанным ID/беседе и сообщением (по умолчанию без него) при выполнении условий из функции "msg" \n \
-10. mailing <text> <userids> <chats=False> - рассылка по ID пользователей/бесед')
-    
+2. get_session - возвращает в переменную сессию в Вконтакте, чтобы при необходимости использования чистого Vk_api не входить и не нагружать сервер второй раз. \n \
+3. give_session <session> - получает уже готовую Вк-сессию, если вы уже в нее вошли через vk_api или другие api для обхода login. \n \
+4. msg <text> <userid> <chats=False> - отправляет текст по указанному ID пользователя/чата, если он уже писал боту/бот состоит в беседе и имеет право писать сообщения. \n \
+5. check_new_msg <chats=False> - проверяет наличие новых сообщений, выдает True/False. \n \
+6. get_event - возвращает в переменную данные о сообщении. \n \
+7. send_photo <file_name> <userid> <msg=None> <chats=False> - отправляет фото с компьютера по указанному адресу пользователю с указанным ID/беседе и сообщением (по умолчанию без него) при выполнении условий из функции "msg". \n \
+8. new_keyboard <dicts> <perm=True> - возвращает в переменную клавиатуру по массиву со словарями, например - [{\'кнопка 1\':\'цвет 1\'}, {\'new_line\':\' \'}, {\'кнопка 2\':\'цвет 2\'}], perm обозначает, будет ли пропадать клавиатура после нажатия на кнопку (при True - не будет). Подробнее можно узнать в документации - github.com/Ma-Mush/LiteVkApi \n \
+9. send_keyboard <keyboard> <userid> <msg=\'Клавиатура!\'> <chats=False> - отправляет пользователю/беседе по ID  указанную клавиатуру, отправляя указанное сообщение \n \
+10. delete_keyboard <userid> <msg=\'Клавиатура закрыта!\'> <chats=False> - удаляет клавиатуру у указанного пользователя, отправляя указанное сообщение \n \
+11. send_file <file_name> <userid> <msg=None> <chats=False> - отправляет файл с компьютера по указанному адресу пользователю с указанным ID/беседе и сообщением (по умолчанию без него) при выполнении условий из функции "msg" \n \
+12. mailing <text> <userids> <chats=False> - рассылка по ID пользователей/бесед \n \
+13. get_all_message_data - возвращает в переменную массив со словарями. В словарях данные о последнем сообщении чата (беседы, ЛС) \n \
+14. get_all_open_id <message_data=None> - возвращает в переменную id всех чатов/пользователей которые писали боту/где он находится.')
+
     def login(id_group, tok, chats=False, my_key=0, my_server=0, my_ts=0):
-        fun = 'help, login, msg, check_new_msg, get_event, send_photo, new_keyboard, send_keyboard, delete_keyboard, send_file.\n\
+        fun = 'help, login, get_session, give_session, msg, check_new_msg, get_event, send_photo, new_keyboard, send_keyboard, delete_keyboard, send_file, mailing, get_all_message_data, get_all_open_id.\n\
 Чтобы узнать подробнее о командах, вызовите функцию help - Vk.help()'
         global longpoll, vk, vk_session, server, ts, key
         import vk_api
@@ -41,7 +51,15 @@ class Vk():
                 server = my_server
                 ts = my_ts
         print('Привет! все мои функии: {}'.format(fun))
-    
+
+    def get_session():
+        global vk_session
+        return vk_session
+
+    def give_session(session):
+        global vk_session
+        vk_session = session
+
     def msg(text, userid, chats=False):
         from vk_api.utils import get_random_id
         try:
@@ -71,10 +89,10 @@ class Vk():
                     return True
         else:
             raise ValueError('Ошибка поиска сообщений (check_new_msg):\n=====\nНе правильно введен параметр chats\n=====')
-    
+
     def get_event():
         return event_msg
-    
+
     def send_photo(file_name, userid, msg=None, chats=False):
         import vk_api
         from vk_api.utils import get_random_id
@@ -93,7 +111,7 @@ class Vk():
                 raise ValueError('Ошибка отправки фото (send_photo):\n=====\nНе правильно введен параметр chats\n=====')
         except:
             raise ValueError('Ошибка отправки фото (send_photo):\n=====\nНе правильно введен один из параметров отправки сообщений\n=====')
-    
+
     def send_file(file_name, userid, msg=None, chats=False):
         import vk_api
         from vk_api.utils import get_random_id
@@ -112,7 +130,7 @@ class Vk():
                 raise ValueError('Ошибка отправки файла (send_file):\n=====\nНе правильно введен параметр chats\n=====')
         except:
             raise ValueError('Ошибка отправки файла (send_file):\n=====\nНе правильно введен один из параметров отправки сообщений\n=====')
-    
+
     def new_keyboard(dicts, perm=True):
         from vk_api.keyboard import VkKeyboard, VkKeyboardColor
         if perm == False:
@@ -166,7 +184,7 @@ class Vk():
             except:
                 raise ValueError('Ошибка создания клавиатуры (new_keyboard):\n=====\nНеправильно указан один из параметров клавиатуры\n=====')
         return keyboard
-    
+
     def send_keyboard(keyboard, userid, msg='Клавиатура!', chats=False):
         from vk_api.utils import get_random_id
         try:
@@ -179,7 +197,7 @@ class Vk():
         except:
             raise ValueError('Ошибка отправки клавиатуры (send_keyboard):\n=====\nНе правильно введен параметр userid/диалог с пользователем не обозначен\
 (бот раньше не писал ему сообщения или не находится в беседе)\n=====')
-    
+
     def delete_keyboard(userid, msg='Клавиатура закрыта!', chats=False):
         from vk_api.keyboard import VkKeyboard
         from vk_api.utils import get_random_id
@@ -195,17 +213,50 @@ class Vk():
         except:
             raise ValueError('Ошибка удаления клавиатуры (delete_keyboard):\n=====\nНе правильно введен параметр userid/диалог с пользователем не обозначен\
 (бот раньше не писал ему сообщения или не находится в беседе)\n=====')
-    def mailing(text, userids, chats=False):
+
+    def mailing(text, userids, safe=[], chats=False):
         try: from threading import Thread
         except: raise ValueError('Ошибка рассылки (mailing):\n=====\nНе установлен модуль threading (Всмысле, он же встроеный...)\n=====')
         def r(text, userids):
             try:
                 for i in userids:
                     try:
-                        Vk.msg(text, i, chats)
+                        if i not in safe:
+                            Vk.msg(text, i, chats)
                     except:
                         1
             except:
                 raise ValueError('Ошибка рассылки (mailing):\n=====\nПередан НЕ массив в значение userids или ошибка отправки сообщения\n=====')
         t = Thread(target=r, args=(text, userids))
         t.start()
+
+    def get_all_message_data():
+        v = vk_session.method("messages.getConversations", {"count": 200})
+        ret = []
+        ch = 0
+        vv = 0
+        for i in v["items"]:
+            ret.append(i["last_message"])
+            ch+=1
+        while True:
+            if ch == 200 or vv == 199:
+                vv = 0
+                v = vk_session.method("messages.getConversations", {"count": 200, "start_message_id":ret[ch-1]["id"]})
+                del v["items"][0]
+                for i in v["items"]:
+                    ret.append(i["last_message"])
+                    ch+=1
+                    vv+=1
+            else:
+                break
+        del ch, vv, v
+        return ret
+
+    def get_all_open_id(message_data=None):
+        if message_data == None:
+            message_data = Vk.get_all_message_data()
+        ret = []
+        for i in message_data:
+            ret.append(i['peer_id'])
+        del message_data
+        return ret
